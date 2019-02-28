4ce1e1d97119835430a2c254c89ee13276d726dc
Haini <constantin.schieber@outlook.com>
2019-02-28 17:06:13 +0100

    Updated documentation

--- sources/bitstream_convert.tcl
+++ sources/bitstream_convert.tcl
@@ -7,6 +7,3 @@ file copy -force -- ./../top_integration_pr/m1_for_arty_a7.runs/child_0_impl_1/g
 write_cfgmem -format BIN -interface SMAPx32 -disablebitswap -loadbit "up 0 ./Bitstreams/cortex_i_cortex_ecu_partial.bit" ./Bitstreams/ecu.bin -force
 
 write_cfgmem -format BIN -interface SMAPx32  -disablebitswap -loadbit "up 0 ./Bitstreams/cortex_i_cortex_throttle_partial.bit" ./Bitstreams/trl.bin -force
-#write_cfgmem -format BIN -disablebitswap -loadbit "up 0 ./Bitstreams/b_led.bit" ./Bitstreams/b_led.bin -force
-
-
